9fa18261caff0956dea39ce94dc0bf321f549d22
Nakst <17091026+nakst@users.noreply.github.com>
2022-02-17 10:04:38 +0000

    add UIDrawTriangle and UIDrawTriangleOutline

--- luigi.h
+++ luigi.h
@@ -644,6 +644,8 @@ void UICodeInsertContent(UICode *code, const char *content, ptrdiff_t byteCount,
 void UIDrawBlock(UIPainter *painter, UIRectangle rectangle, uint32_t color);
 void UIDrawInvert(UIPainter *painter, UIRectangle rectangle);
 bool UIDrawLine(UIPainter *painter, int x0, int y0, int x1, int y1, uint32_t color); // Returns false if the line was not visible.
+void UIDrawTriangle(UIPainter *painter, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+void UIDrawTriangleOutline(UIPainter *painter, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
 void UIDrawGlyph(UIPainter *painter, int x, int y, int c, uint32_t color);
 void UIDrawRectangle(UIPainter *painter, UIRectangle r, uint32_t mainColor, uint32_t borderColor, UIRectangle borderSize);
 void UIDrawBorder(UIPainter *painter, UIRectangle r, uint32_t borderColor, UIRectangle borderSize);
@@ -1201,6 +1203,56 @@ bool UIDrawLine(UIPainter *painter, int x0, int y0, int x1, int y1, uint32_t col
 	}
 
 	return true;
+}
+
+void UIDrawTriangle(UIPainter *painter, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+	// Step 1: Sort the points by their y-coordinate.
+	if (y1 < y0) { int xt = x0; x0 = x1, x1 = xt; int yt = y0; y0 = y1, y1 = yt; }
+	if (y2 < y1) { int xt = x1; x1 = x2, x2 = xt; int yt = y1; y1 = y2, y2 = yt; }
+	if (y1 < y0) { int xt = x0; x0 = x1, x1 = xt; int yt = y0; y0 = y1, y1 = yt; }
+	if (y2 == y0) return;
+
+	// Step 2: Clip the triangle.
+	if (x0 < painter->clip.l && x1 < painter->clip.l && x2 < painter->clip.l) return;
+	if (x0 >= painter->clip.r && x1 >= painter->clip.r && x2 >= painter->clip.r) return;
+	if (y2 < painter->clip.t || y0 >= painter->clip.b) return;
+	bool needsXClip = x0 < painter->clip.l + 1 || x0 >= painter->clip.r - 1
+		|| x1 < painter->clip.l + 1 || x1 >= painter->clip.r - 1
+		|| x2 < painter->clip.l + 1 || x2 >= painter->clip.r - 1;
+	bool needsYClip = y0 < painter->clip.t + 1 || y2 >= painter->clip.b - 1;
+#define _UI_DRAW_TRIANGLE_APPLY_CLIP(xo, yo) \
+	if (needsYClip && (yi + yo < painter->clip.t || yi + yo >= painter->clip.b)) continue; \
+	if (needsXClip && xf + xo < painter->clip.l) xf = painter->clip.l - xo; \
+	if (needsXClip && xt + xo > painter->clip.r) xt = painter->clip.r - xo;
+
+	// Step 3: Split into 2 triangles with bases aligned with the x-axis.
+	float xm0 = (x2 - x0) * (y1 - y0) / (y2 - y0), xm1 = x1 - x0;
+	if (xm1 < xm0) { float xmt = xm0; xm0 = xm1, xm1 = xmt; }
+	float xe0 = xm0 + x0 - x2, xe1 = xm1 + x0 - x2;
+	int ym = y1 - y0, ye = y2 - y1;
+	float ymr = 1.0f / ym, yer = 1.0f / ye;
+
+	// Step 4: Draw the top part.
+	for (float y = 0; y < ym; y++) {
+		int xf = xm0 * y * ymr, xt = xm1 * y * ymr, yi = (int) y;
+		_UI_DRAW_TRIANGLE_APPLY_CLIP(x0, y0);
+		uint32_t *b = &painter->bits[(yi + y0) * painter->width + x0];
+		for (int x = xf; x < xt; x++) b[x] = color;
+	}
+
+	// Step 5: Draw the bottom part.
+	for (float y = 0; y < ye; y++) {
+		int xf = xe0 * (ye - y) * yer, xt = xe1 * (ye - y) * yer, yi = (int) y;
+		_UI_DRAW_TRIANGLE_APPLY_CLIP(x2, y1);
+		uint32_t *b = &painter->bits[(yi + y1) * painter->width + x2];
+		for (int x = xf; x < xt; x++) b[x] = color;
+	}
+}
+
+void UIDrawTriangleOutline(UIPainter *painter, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+	UIDrawLine(painter, x0, y0, x1, y1, color);
+	UIDrawLine(painter, x1, y1, x2, y2, color);
+	UIDrawLine(painter, x2, y2, x0, y0, color);
 }
 
 void UIDrawInvert(UIPainter *painter, UIRectangle rectangle) {
