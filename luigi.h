bb4ac5f95507625b025913757e225130e43e634b
Nakst <17091026+nakst@users.noreply.github.com>
2022-03-06 19:42:54 +0000

    Update luigi.h

--- luigi.h
+++ luigi.h
@@ -588,6 +588,10 @@ typedef struct UIImageDisplay {
 	int previousPanPointX, previousPanPointY;
 } UIImageDisplay;
 
+typedef struct UIWrapPanel {
+	UIElement e;
+} UIWrapPanel;
+
 void UIInitialise();
 int UIMessageLoop();
 
@@ -607,6 +611,7 @@ UISlider *UISliderCreate(UIElement *parent, uint32_t flags);
 UISpacer *UISpacerCreate(UIElement *parent, uint32_t flags, int width, int height);
 UISplitPane *UISplitPaneCreate(UIElement *parent, uint32_t flags, float weight);
 UITabPane *UITabPaneCreate(UIElement *parent, uint32_t flags, const char *tabs /* separate with \t, terminate with \0 */);
+UIWrapPanel *UIWrapPanelCreate(UIElement *parent, uint32_t flags);
 
 UILabel *UILabelCreate(UIElement *parent, uint32_t flags, const char *label, ptrdiff_t labelBytes);
 void UILabelSetContent(UILabel *code, const char *content, ptrdiff_t byteCount);
@@ -1763,6 +1768,68 @@ UIPanel *UIPanelCreate(UIElement *parent, uint32_t flags) {
 	}
 
 	return panel;
+}
+
+void _UIWrapPanelLayoutRow(UIWrapPanel *panel, UIElement *child, UIElement *rowEnd, int rowY, int rowHeight) {
+	int rowPosition = 0;
+
+	while (child != rowEnd) {
+		int height = UIElementMessage(child, UI_MSG_GET_HEIGHT, 0, 0);
+		int width = UIElementMessage(child, UI_MSG_GET_WIDTH, 0, 0);
+		UIRectangle relative = UI_RECT_4(rowPosition, rowPosition + width, rowY + rowHeight / 2 - height / 2, rowY + rowHeight / 2 + height / 2);
+		UIElementMove(child, UIRectangleTranslate(relative, panel->e.bounds), false);
+		child = child->next;
+		rowPosition += width;
+	}
+}
+
+int _UIWrapPanelMessage(UIElement *element, UIMessage message, int di, void *dp) {
+	UIWrapPanel *panel = (UIWrapPanel *) element;
+	bool horizontal = element->flags & UI_PANEL_HORIZONTAL;
+
+	if (message == UI_MSG_LAYOUT || message == UI_MSG_GET_HEIGHT) {
+		int totalHeight = 0;
+		int rowPosition = 0;
+		int rowHeight = 0;
+		int rowLimit = message == UI_MSG_LAYOUT ? UI_RECT_WIDTH(element->bounds) : di;
+
+		UIElement *child = panel->e.children;
+		UIElement *rowStart = child;
+
+		while (child) {
+			if (~child->flags & UI_ELEMENT_HIDE) {
+				int height = UIElementMessage(child, UI_MSG_GET_HEIGHT, 0, 0);
+				int width = UIElementMessage(child, UI_MSG_GET_WIDTH, 0, 0);
+
+				if (rowLimit && rowPosition + width > rowLimit) {
+					_UIWrapPanelLayoutRow(panel, rowStart, child, totalHeight, rowHeight);
+					totalHeight += rowHeight;
+					rowPosition = rowHeight = 0;
+					rowStart = child;
+				}
+
+				if (height > rowHeight) {
+					rowHeight = height;
+				}
+
+				rowPosition += width;
+			}
+
+			child = child->next;
+		}
+
+		if (message == UI_MSG_GET_HEIGHT) {
+			return totalHeight + rowHeight;
+		} else {
+			_UIWrapPanelLayoutRow(panel, rowStart, child, totalHeight, rowHeight);
+		}
+	}
+
+	return 0;
+}
+
+UIWrapPanel *UIWrapPanelCreate(UIElement *parent, uint32_t flags) {
+	return (UIWrapPanel *) UIElementCreate(sizeof(UIWrapPanel), parent, flags, _UIWrapPanelMessage, "Wrap Panel");
 }
 
 void _UIButtonCalculateColors(UIElement *element, uint32_t *color, uint32_t *textColor) {
